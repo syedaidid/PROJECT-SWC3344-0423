70ee70f0e79d25282005c04b9de48fc19d83ae79
Syed Aidid <syedaidid584@gmail.com>
2023-07-03 03:04:37 +0800

    add comment

--- Gui.java
+++ Gui.java
@@ -33,64 +33,69 @@ public class Gui implements ActionListener {
 
 		// Panel Counter 1
 		panelCounter1 = new JPanel();
-		panelCounter1.setBounds(20, 60, 110, 50);
-		panelCounter1.setBorder(new LineBorder(Color.BLACK));
-		JLabel labelCounter1 = new JLabel("Counter 1");
-		labelCounter1.setFont(new Font("Verdana", 1, 15));
-		labelQueue1 = new JLabel("Queue: " + String.valueOf(queue1));
+		panelCounter1.setBounds(20, 60, 110, 50); // set the size and position
+		panelCounter1.setBorder(new LineBorder(Color.BLACK)); // create border
+		JLabel labelCounter1 = new JLabel("Counter 1"); // create label
+		labelCounter1.setFont(new Font("Verdana", 1, 15)); // change font
+		labelQueue1 = new JLabel("Queue: " + String.valueOf(queue1)); // create label for how many customer in counter
+																		// queue left
 		panelCounter1.add(labelCounter1);
 		panelCounter1.add(labelQueue1);
 
 		// Panel Counter 2
 		panelCounter2 = new JPanel();
-		panelCounter2.setBounds(150, 60, 110, 50);
-		panelCounter2.setBorder(new LineBorder(Color.BLACK));
-		JLabel labelCounter2 = new JLabel("Counter 2");
-		labelCounter2.setFont(new Font("Verdana", 1, 15));
-		labelQueue2 = new JLabel("Queue: " + String.valueOf(queue2));
+		panelCounter2.setBounds(150, 60, 110, 50); // size and position
+		panelCounter2.setBorder(new LineBorder(Color.BLACK)); // create border
+		JLabel labelCounter2 = new JLabel("Counter 2"); // create label
+		labelCounter2.setFont(new Font("Verdana", 1, 15)); // change font
+		labelQueue2 = new JLabel("Queue: " + String.valueOf(queue2)); // create label for how many customer in counter
+																		// queue left
 		panelCounter2.add(labelCounter2);
 		panelCounter2.add(labelQueue2);
 
 		// Panel Counter 3
 		panelCounter3 = new JPanel();
-		panelCounter3.setBounds(280, 60, 110, 50);
-		panelCounter3.setBorder(new LineBorder(Color.BLACK));
-		JLabel labelCounter3 = new JLabel("Counter 3");
-		labelCounter3.setFont(new Font("Verdana", 1, 15));
-		labelQueue3 = new JLabel("Queue: " + String.valueOf(queue3));
+		panelCounter3.setBounds(280, 60, 110, 50); // size and position
+		panelCounter3.setBorder(new LineBorder(Color.BLACK)); // create border
+		JLabel labelCounter3 = new JLabel("Counter 3"); // create label
+		labelCounter3.setFont(new Font("Verdana", 1, 15)); // change font
+		labelQueue3 = new JLabel("Queue: " + String.valueOf(queue3)); // create label for how many customer in counter
+																		// queue left
 		panelCounter3.add(labelCounter3);
 		panelCounter3.add(labelQueue3);
 
 		// Button pay counter 1
 		btnPay1 = new JButton("PAY");
-		btnPay1.setBounds(40, 25, 75, 30);
+		btnPay1.setBounds(40, 25, 75, 30); // size and position
 
 		// Button pay counter 1
 		btnPay2 = new JButton("PAY");
-		btnPay2.setBounds(170, 25, 75, 30);
+		btnPay2.setBounds(170, 25, 75, 30); // size and position
 
 		// Button pay counter 1
 		btnPay3 = new JButton("PAY");
-		btnPay3.setBounds(300, 25, 75, 30);
+		btnPay3.setBounds(300, 25, 75, 30); // size and position
 
 		// Button NEXT
 		btnNext = new JButton("NEXT");
-		btnNext.setBounds(170, 200, 75, 30);
-		labelCustomerLeft = new JLabel("Customers Left: " + String.valueOf(totalCustomer));
-		labelCustomerLeft.setBounds(150, 230, 200, 20);
+		btnNext.setBounds(170, 200, 75, 30); // size and position
+		labelCustomerLeft = new JLabel("Customers Left: " + String.valueOf(totalCustomer)); // create label to show how
+																							// many customers left to
+																							// pay
+		labelCustomerLeft.setBounds(150, 230, 200, 20); // size and position
 
 		// Button to display completed customer
 		btnDisplay = new JButton("DISPLAY");
-		btnDisplay.setBounds(162, 270, 90, 30);
+		btnDisplay.setBounds(162, 270, 90, 30); // size and position
 
 		// Frame
 		frame = new JFrame();
-		frame.setTitle("HyperMarket");
-		frame.setLayout(null);
+		frame.setTitle("HyperMarket"); // Frame title
+		frame.setLayout(null); // frame layout
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(430, 400);
+		frame.setSize(430, 400); // size
 		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null); // spawn in the middle of screen
 		frame.add(panelCounter1);
 		frame.add(panelCounter2);
 		frame.add(panelCounter3);
@@ -101,6 +106,7 @@ public class Gui implements ActionListener {
 		frame.add(labelCustomerLeft);
 		frame.add(btnDisplay);
 
+		// Action Listener
 		btnPay1.addActionListener(this);
 		btnPay2.addActionListener(this);
 		btnPay3.addActionListener(this);
@@ -110,7 +116,11 @@ public class Gui implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
+
+		// Iterator. Use to iterate or scan through the list
 		Iterator<CustomerInformation> iterator = customerList.iterator();
+
+		// create instance
 		CustomerInformation cust;
 		if (e.getSource() == btnNext) { // start btnNext
 
@@ -118,8 +128,11 @@ public class Gui implements ActionListener {
 			while (iterator.hasNext()) {
 				CustomerInformation customer = iterator.next();
 
+				// If the customer's item is <= 5, the customer will go counter 1, if full, go
+				// to counter 2, if full also, wait for the queue to be available
 				if (customer.getItemQuantity() <= 5) {
 					if (qCounter1.size() < 5) {
+						// remove customer
 						iterator.remove();
 						qCounter1.enqueue(customer);
 						customer.setCounterPaid(1);
